b619d4002289b2499d596e3a655ee9675af8de26
Leo Allen <leo.f.allen@gmail.com>
2013-05-17 12:51:49 +0100

    Added validations to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,20 @@
 class UsersController < ApplicationController
   def new
-
+    @user = User.new
   end
 
   def create
-    User.create(params[:user])
-    render :json => {:message => "Success!"}
+    @user = User.create(params[:user])
+    render :json => create_json
+  end
+
+  private
+
+  def create_json
+    if @user.invalid?
+      {:message => "There were some errors in your form", :errors => @user.errors}
+    else
+      {:message => "Success!"}
+    end
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,6 +4,8 @@ describe UsersController do
 
   let(:user_hash) { {:user => {:name => "Dave", :email => "dave@example.com", :age => 23}} }
 
+  let(:invalid_user_hash) { {:user => {:name => "Blah", :age => 24}}}
+
   context "GET new" do
     it "renders the user new form" do
       get :new
@@ -12,13 +14,33 @@ describe UsersController do
   end
 
   context "POST create" do
-    it "creates a new user" do
-      expect {post :create, user_hash}.to change(User, :count).by(1)
+    context "valid attributes" do
+
+      let(:post_success) { {"message" => "Success!"} }
+
+      it "creates a new user" do
+        expect {post :create, user_hash}.to change(User, :count).by(1)
+      end
+
+      it "returns a successful response" do
+        post :create, user_hash
+        parsed_response.should eq post_success
+      end
     end
 
-    it "returns a successful response" do
-      post :create, user_hash
-      response.body.should eq '{"message":"Success!"}'
+    context "invalid attributes" do
+
+      let(:post_fail) { {"message" => "There were some errors in your form", "errors" => {"email" => ["can't be blank"]}} }
+
+      it "responds with a json error" do
+        post :create, invalid_user_hash
+
+        parsed_response.should eq post_fail
+      end
     end
+  end
+
+  def parsed_response
+    JSON.parse(response.body)
   end
 end
